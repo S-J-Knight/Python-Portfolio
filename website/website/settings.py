6c1c6b4c885e2fe108c372bd02514af16198eac7
Samuel Knight <132781024+S-J-Knight@users.noreply.github.com>
2025-11-02 21:37:37 +0000

    Rebrand to Knightcycle, rename send_waste to recycle_and_earn, update logo to v2, fix os import

--- website/website/settings.py
+++ website/website/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
-from pathlib import os
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
