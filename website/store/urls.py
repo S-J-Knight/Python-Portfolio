25a4b9a22d4b4b0d69441e1e6bb8f3823c8ee078
Samuel Knight <132781024+S-J-Knight@users.noreply.github.com>
2025-12-04 11:49:23 +0000

    Fix password reset email subject to say Knightcycle instead of example.com

--- website/store/urls.py
+++ website/store/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('password-reset/', auth_views.PasswordResetView.as_view(
         template_name='store/password_reset.html',
         email_template_name='registration/password_reset_email.html',
+        subject_template_name='registration/password_reset_subject.txt',
         success_url='/store/password-reset/done/'
     ), name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='store/password_reset_done.html'), name='password_reset_done'),
